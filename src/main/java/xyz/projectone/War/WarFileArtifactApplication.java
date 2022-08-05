86b75b3d304185c3b5b68c40f88837473ac8d951
joshua-demebo <78673701+joshua-demebo@users.noreply.github.com>
2022-08-05 04:27:55 +0000

    Correct

--- src/main/java/xyz/projectone/War/WarFileArtifactApplication.java
+++ src/main/java/xyz/projectone/War/WarFileArtifactApplication.java
@@ -4,6 +4,8 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
+@ComponentScan(basePackages ="xyz.projectone.War,"
+	       + "xyz.projectone.War.service")
 public class WarFileArtifactApplication {
 
 	public static void main(String[] args) {
