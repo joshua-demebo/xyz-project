a2ece5d32a4862b7ac5113b6360a09c59b398574
Joshua Demebo <joshua.demebo@live.com>
2022-08-05 01:18:49 +0000

    all files to git

---
+++ src/main/java/xyz/projectone/War/WarFileArtifactApplication.java
@@ -0,0 +1,13 @@
+package xyz.projectone.War;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WarFileArtifactApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WarFileArtifactApplication.class, args);
+	}
+
+}
---
+++ src/test/java/xyz/projectone/War/WarFileArtifactApplicationTests.java
@@ -0,0 +1,13 @@
+package xyz.projectone.War;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WarFileArtifactApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
